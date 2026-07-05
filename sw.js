ee672e72abd44fd0c01a51a279552fd2b082f845
miscojones3434 <miscojones424@gmail.com>
2026-07-05 21:55:32 +0200

    Add service worker for caching fetch requests

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'pt34-fy-v1';
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
